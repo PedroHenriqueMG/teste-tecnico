70f89b6990fe8eb37e01aaf36669437ff5ee75bb
PedroHenriqueMG <pedrohdefsouza@gmail.com>
2024-06-09 13:04:52 -0300

    feat: create express router

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,10 @@
 import Express from "express";
+import { route } from "./routes";
 
 const api = Express();
 api.use(Express.json());
+
+api.use(route);
 
 api.listen(process.env.PORT, () => {
   console.log(`API rodando na porta: ${process.env.PORT}`);
